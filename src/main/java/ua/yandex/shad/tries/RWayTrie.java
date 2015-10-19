d22daee68743e3f31109242d878d236939057e65
Romm17 <romanysik@gmail.com>
2015-10-19 15:29:18 +0300

    v1.0

---
+++ src/main/java/ua/yandex/shad/tries/RWayTrie.java
@@ -0,0 +1,228 @@
+package ua.yandex.shad.tries;
+
+import java.util.ArrayList;
+
+public class RWayTrie implements Trie {
+    
+    public static int R = 26;
+    
+//    private final String[][] base;
+    private Node[] startNodes;
+    
+//    private Node root;
+    
+    private int size;
+    
+    public static class Node {
+        
+        public boolean val;
+        public char c;
+        public Node left, middle, right;
+        
+        public Node(){
+            
+            val = false;
+            left = middle = right = null;
+            
+        }
+        
+        public Node(char c){
+            
+            this.c = c;
+            val = false;
+            left = middle = right = null;
+            
+        }
+        
+    }
+    
+    public RWayTrie(){
+//        this.base = new String[R][R];
+//        for(int i = 0; i < R; i++){
+//            for(int j = 0; j < R; j++){
+//                base[i][j] = "" + (char)('a' + i) + (char)('a' + j);
+//            }
+//        }
+        this.startNodes = new Node[R * R];
+        size = 0;
+        
+    }
+    
+//    public String getBase(int i, int j){
+//        return base[i][j];
+//    }
+    
+    private Node add(Node x, String s, int k){
+        if(x == null){
+            x = new Node(s.charAt(k));
+        }
+        if(s.charAt(k) < x.c){
+            x.left = add(x.left, s, k);
+        }
+        else if(s.charAt(k) > x.c){
+            x.right = add(x.right, s, k);
+        }
+        else if(k < s.length() - 1){
+            x.middle = add(x.middle, s, k + 1);
+        }
+        else{
+            x.val = true;
+            size++;
+        }
+        return x;
+    }
+
+    @Override
+    public void add(Tuple t) {
+        Node x = this.startNodes[R * (t.term.charAt(0) - 'a') 
+                + (t.term.charAt(1) - 'a')];
+        this.startNodes[R * (t.term.charAt(0) - 'a') 
+                + (t.term.charAt(1) - 'a')] = add(x, t.term, 2);
+    }
+    
+    private boolean contains(Node x, String s, int k){
+        if(x == null)
+            return false;
+        if(s.charAt(k) > x.c){
+            return contains(x.right, s, k);
+        }
+        else if(s.charAt(k) < x.c){
+            return contains(x.left, s, k);
+        }
+        else if(s.length() - 1 > k){
+            return contains(x.middle, s, k + 1);
+        }
+        else{
+            return x.val;
+        }
+    }
+
+    @Override
+    public boolean contains(String word) {
+        Node x = this.startNodes[R * (word.charAt(0) - 'a') 
+                + (word.charAt(1) - 'a')];
+        return contains(x, word, 2);
+    }
+    
+    private Node delete(Node x, String s, int k){
+        if(x == null)
+            return x;
+        if(s.charAt(k) > x.c){
+            x.right = delete(x.right, s, k);
+        }
+        else if(s.charAt(k) < x.c){
+            x.left = delete(x.left, s, k);
+        }
+        else if(s.length() - 1 > k){
+            x.middle = delete(x.middle, s, k + 1);
+        }
+        else{
+            if(!x.val)
+                return x;
+            if(x.left == null
+                    && x.right == null
+                    && x.middle == null){
+                x = null;
+            }
+            else{
+                x.val = false;
+            }
+            size--;
+        }
+        return x;
+    }
+
+    @Override
+    public boolean delete(String word) {
+        int sizeWas = size;
+        Node x = this.startNodes[R * (word.charAt(0) - 'a') 
+                + (word.charAt(1) - 'a')];
+        this.startNodes[R * (word.charAt(0) - 'a') 
+                + (word.charAt(1) - 'a')] = delete(x, word, 2);
+        int sizeNow = size;
+        return sizeWas - sizeNow == 1;
+    }
+    
+    public void findWords(Node x, String s, ArrayList<String> arr){
+        if(x == null)
+            return;
+        findWords(x.left, s, arr);
+        if(x.val){
+            arr.add(s + x.c);
+        }
+        findWords(x.middle, s + x.c, arr);
+        findWords(x.right, s, arr);
+    }
+
+    @Override
+    public Iterable<String> words() {
+        ArrayList<String> words = new ArrayList();
+        for(int i = 0; i < R; i++){
+            for(int j = 0; j < R; j++){
+                Node x = this.startNodes[R * i + j];
+                findWords(x, "" + (char)('a' + i) + (char)('a' + j), words);
+            }
+        }
+        return words;
+    }
+    
+    public void findWordsWithPrefix(Node x, String s, String prefix, 
+            int k, ArrayList<String> arr
+            ){
+        if(x == null)
+            return;
+        if(k < prefix.length()){
+            if(prefix.charAt(k) < x.c){
+                this.findWordsWithPrefix(x.left, s, prefix, k, arr);
+            }
+            else if(prefix.charAt(k) > x.c){
+                this.findWordsWithPrefix(x.right, s, prefix, k, arr);
+            }
+            else {
+                if(k == prefix.length() - 1 && x.val){
+                    arr.add(prefix);
+                }
+                this.findWordsWithPrefix(x.middle, s + x.c, prefix, k + 1, arr);
+            }      
+        }
+        else{
+            findWordsWithPrefix(x.left, s, prefix, k, arr);
+            if(x.val){
+                arr.add(s + x.c);
+            }
+            findWordsWithPrefix(x.middle, s + x.c, prefix, k + 1, arr);
+            findWordsWithPrefix(x.right, s, prefix, k, arr);
+        }
+    }
+
+    @Override
+    public Iterable<String> wordsWithPrefix(String s) {
+        if(s.length() == 0){
+            return new ArrayList();
+        }
+        ArrayList<String> words = new ArrayList();
+        Node x;
+        if(s.length() == 1){
+            for(int i = 0; i < R; i++){
+                x = this.startNodes[R * (s.charAt(0) - 'a') 
+                    + i];
+                String prefix = "" + s.charAt(0) + (char)('a' + i);
+                findWordsWithPrefix(x, prefix,
+                        s, 2, words);
+            }
+        }
+        else {
+            x = this.startNodes[R * (s.charAt(0) - 'a') 
+                    + (s.charAt(1) - 'a')];
+            String prefix = "" + s.charAt(0) + s.charAt(1);
+            findWordsWithPrefix(x, prefix, s, 2, words);
+        }
+        return words;
+    }
+
+    @Override
+    public int size() {
+        return size; 
+    }
+
+}
---
+++ src/test/java/ua/yandex/shad/tries/RWayTrieTest.java
@@ -0,0 +1,85 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ua.yandex.shad.tries;
+
+import java.util.Iterator;
+import org.junit.Test;
+import org.junit.Assert;
+import org.junit.Ignore;
+
+/**
+ *
+ * @author romm
+ */
+public class RWayTrieTest {
+    
+    @Ignore
+    @Test
+    public void testRWayTrie(){
+        RWayTrie trie = new RWayTrie();
+        for(int i = 0; i < 26; i++){
+            for(int j = 0; j < 26; j++){
+                String expected = "" + (char)('a' + i) + (char)('a' + j);
+                Assert.assertEquals(trie.getBase(i, j), expected);
+            }
+        }
+    }
+    
+    @Test
+    public void testAdd(){
+        RWayTrie trie = new RWayTrie();
+        trie.add(new Tuple("lololo", 6));
+        trie.add(new Tuple("lololk", 6));
+        trie.add(new Tuple("lolok", 5));
+        trie.add(new Tuple("loloa", 5));
+        Assert.assertTrue(trie.contains("lololo"));
+        Assert.assertTrue(trie.contains("lololk"));
+        Assert.assertTrue(trie.contains("lolok"));
+        Assert.assertTrue(trie.contains("loloa"));
+//        Assert.assertTrue(true);
+    }
+    
+    @Test
+    public void testDelete(){
+        RWayTrie trie = new RWayTrie();
+        trie.add(new Tuple("lololo", 6));
+        trie.add(new Tuple("lololk", 6));
+        trie.add(new Tuple("lolok", 5));
+        trie.add(new Tuple("loloa", 5));
+        Assert.assertTrue(trie.contains("lolok"));
+        trie.delete("lolok");
+        Assert.assertEquals(trie.size(), 3);
+        Assert.assertFalse(trie.contains("lolok"));
+    }
+    
+    @Test
+    public void testWords(){
+        RWayTrie trie = new RWayTrie();
+        trie.add(new Tuple("lololo", 6));
+        trie.add(new Tuple("lololk", 6));
+        trie.add(new Tuple("lolok", 5));
+        trie.add(new Tuple("loloa", 5));
+        Iterable<String> words = trie.words();
+//        for(String s : words){
+//            System.out.println(s);
+//        }
+    }
+    @Test
+    public void testWordsWithPrefix(){
+        RWayTrie trie = new RWayTrie();
+        trie.add(new Tuple("lololo", 6));
+        trie.add(new Tuple("lololk", 6));
+        trie.add(new Tuple("laloa", 5));
+        trie.add(new Tuple("lolok", 5));
+        trie.add(new Tuple("loloa", 5));
+        Iterable<String> words = trie.wordsWithPrefix("");
+        for(String s : words){
+            System.out.println(s);
+        }
+    }
+    
+    
+}
